cab7ccdceff00102941449581e3489ed67b191cf
Lovin Sharma <130526452+Shawarm0@users.noreply.github.com>
2026-01-25 14:06:18 +0000

    Initial Commit

---
+++ ChoreTracker/ChoreTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChoreTrackerApp.swift
+//  ChoreTracker
+//
+//  Created by Lovin Sharma on 25/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct ChoreTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
